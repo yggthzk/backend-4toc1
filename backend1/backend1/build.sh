1c09d293c33b73632f2d58a6a0835045a276924d
bgrey77 <benjamin.reyes34@inacapmail.cl>
2025-10-31 14:22:12 -0300

    builds,render

--- backend1/backend1/build.sh
+++ backend1/backend1/build.sh
@@ -1,10 +1,6 @@
 
-#!/usr/bin/env bash
-
 set -o errexit
 
 pip install -r requirements.txt
-
-#'backend1' para ejecutar manage.pyy
-python backend1/manage.py collectstatic --no-input
-python backend1/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
