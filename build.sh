4acb12e395181929ebcf2ea15d76ac9ff933577b
bgrey77 <benjamin.reyes34@inacapmail.cl>
2025-10-31 15:09:24 -0300

    rendercito

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
-python manage.py migrate
+
+python backend1/manage.py collectstatic --no-input
+python backend1/manage.py migrate
