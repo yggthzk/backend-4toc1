29c6756931d1e3a45908ed448f3ddc75daa06bb2
bgrey77 <bgreyesq@gmail.com>
2025-10-31 16:08:03 -0300

    montna

--- backend1/backend1/wsgi.py
+++ backend1/backend1/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# RUTA CORREGIDA
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend1.backend1.settings')
 
 application = get_wsgi_application()
