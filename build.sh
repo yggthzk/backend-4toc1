cca4a031f5659cb4bc27f362d4910b998e59d58c
bgrey77 <benjamin.reyes34@inacapmail.cl>
2025-10-31 14:10:52 -0300

    correcion settings

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 
+#!/usr/bin/env bash
+
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
-python manage.py migrate
+#'backend1' para ejecutar manage.pyy
+python backend1/manage.py collectstatic --no-input
+python backend1/manage.py migrate
